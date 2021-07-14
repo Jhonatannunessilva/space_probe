d674094f471f981882477fab79dc63eab8568c97
Jhonatan Nunes da Silva <jhonatannunessilva@hotmail.com>
2021-07-14 18:53:19 -0300

    added functions to create, update and get a probe

---
+++ lib/space_probe/probes/probes.ex
@@ -0,0 +1,24 @@
+defmodule SpaceProbe.Probes do
+  alias SpaceProbe.Repo
+  alias SpaceProbe.Probes.Schema.Probe
+
+  def update_probe(changes, probe) do
+    probe
+    |> Probe.changeset(changes)
+    |> Repo.update()
+  end
+
+  def get_probe() do
+    case Repo.all(Probe) do
+      [probe | _] ->
+        {:ok, probe}
+
+      _ ->
+        {:error, "Probe not found!"}
+    end
+  end
+
+  def create_probe(params \\ %{}) do
+    params |> Probe.changeset() |> Repo.insert()
+  end
+end

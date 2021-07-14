60d9dc0d03c087d07d190ddea5fd5491523f4eb6
Jhonatan Nunes da Silva <jhonatannunessilva@hotmail.com>
2021-07-14 18:58:15 -0300

    added execute_instructions function

--- lib/space_probe/probes/probes.ex
+++ lib/space_probe/probes/probes.ex
@@ -2,6 +2,36 @@ defmodule SpaceProbe.Probes do
   alias SpaceProbe.Repo
   alias SpaceProbe.Probes.Schema.Probe
 
+  def execute_instructions(probe, instructions) do
+    instructions
+    |> Enum.reduce(%{x: probe.x, y: probe.y, face: probe.face}, fn
+      "M", acc -> move(acc)
+      instruction, acc -> rotate(acc, instruction)
+    end)
+    |> update_probe(probe)
+  end
+
+  defp rotate(%{face: "R"} = changes, "TL"), do: %{changes | face: "U"}
+  defp rotate(%{face: "R"} = changes, "TR"), do: %{changes | face: "D"}
+
+  defp rotate(%{face: "U"} = changes, "TL"), do: %{changes | face: "L"}
+  defp rotate(%{face: "U"} = changes, "TR"), do: %{changes | face: "R"}
+
+  defp rotate(%{face: "L"} = changes, "TL"), do: %{changes | face: "D"}
+  defp rotate(%{face: "L"} = changes, "TR"), do: %{changes | face: "U"}
+
+  defp rotate(%{face: "D"} = changes, "TL"), do: %{changes | face: "R"}
+  defp rotate(%{face: "D"} = changes, "TR"), do: %{changes | face: "L"}
+
+  defp move(%{face: "R", x: x} = changes), do: %{changes | x: x + 1}
+  defp move(%{face: "U", y: y} = changes), do: %{changes | y: y + 1}
+  defp move(%{face: "L", x: x} = changes), do: %{changes | x: x - 1}
+  defp move(%{face: "D", y: y} = changes), do: %{changes | y: y - 1}
+
+  def reset_position(probe) do
+    update_probe(%{x: 0, y: 0, face: "R"}, probe)
+  end
+
   def update_probe(changes, probe) do
     probe
     |> Probe.changeset(changes)

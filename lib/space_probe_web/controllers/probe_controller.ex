be96ed76f13921692e747adeb094dcea658b827e
Jhonatan Nunes da Silva <jhonatannunessilva@hotmail.com>
2021-07-15 08:58:10 -0300

    added probe controller

---
+++ lib/space_probe_web/controllers/probe_controller.ex
@@ -0,0 +1,30 @@
+defmodule SpaceProbeWeb.ProbeController do
+  use SpaceProbeWeb, :controller
+
+  alias SpaceProbe.Probes
+
+  def show(conn, _params) do
+    with {:ok, probe} <- Probes.get_probe() do
+      conn
+      |> put_status(:ok)
+      |> render("show.json", probe: probe)
+    end
+  end
+
+  def execute_instructions(conn, %{"instructions" => instructions}) do
+    with {:ok, probe} <- Probes.get_probe(),
+         {:ok, probe} <- Probes.execute_instructions(probe, instructions) do
+      conn
+      |> put_status(:ok)
+      |> render("show_coordinates.json", probe: probe)
+    end
+  end
+
+  def reset_position(conn, _params) do
+    with {:ok, probe} <- Probes.get_probe(),
+         {:ok, _} <- Probes.reset_position(probe) do
+      conn
+      |> send_resp(:no_content, "")
+    end
+  end
+end
